4a9ded4ac742e735421d5438f7f032a8820b4f47
batsalee <109213754+batsalee@users.noreply.github.com>
2023-06-23 18:19:24 +0900

    Create 트리순회.cpp

---
+++ 그래프이론/트리순회.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> adj[1004];
+int visited[1004];
+
+void postOrder(int here) { // 후위순회
+	if (visited[here] == 0) {
+		if (adj[here].size() == 1)postOrder(adj[here][0]);
+		if (adj[here].size() == 2) {
+			postOrder(adj[here][0]);
+			postOrder(adj[here][1]);
+		}
+		visited[here] = 1;
+		cout << here << ' ';
+	}
+}
+
+void preOrder(int here) { // 전위순회
+	if (visited[here] == 0) {
+		visited[here] = 1;
+		cout << here << ' ';
+		if (adj[here].size() == 1)preOrder(adj[here][0]);
+		if (adj[here].size() == 2) {
+			preOrder(adj[here][0]);
+			preOrder(adj[here][1]);
+		}
+	}
+}
+
+void inOrder(int here) { // 중위순회
+	if (visited[here] == 0) {
+		if (adj[here].size() == 1) {
+			inOrder(adj[here][0]);
+			visited[here] = 1;
+			cout << here << ' ';
+		}
+		else if (adj[here].size() == 2) {
+			inOrder(adj[here][0]);
+
+			visited[here] = 1;
+			cout << here << ' ';
+
+			inOrder(adj[here][1]);
+		}
+		else {
+			visited[here] = 1;
+			cout << here << ' ';
+		}
+	}
+
+}
+
+int main() {
+	adj[1].push_back(2);
+	adj[1].push_back(3);
+	adj[2].push_back(4);
+	adj[2].push_back(5);
+	int root = 1;
+
+	cout << "\n 트리순회 : postOrder \n";
+	postOrder(root); memset(visited, 0, sizeof(visited));
+
+	cout << "\n 트리순회 : preOrder \n";
+	preOrder(root); memset(visited, 0, sizeof(visited));
+
+	cout << "\n 트리순회 : inOrder \n";
+	inOrder(root);
+
+	return 0;
+}
